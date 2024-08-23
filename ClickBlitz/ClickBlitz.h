cf5dffffe04f5dbe3db6eb9321154bf6bd9755e9
David Jonjic <009davide@gmail.com>
2024-08-24 00:44:46 +0200

    Added Windows XP build support.
    This commit adds Windows XP build support by switching the platform toolset to v141_xp. It also changes the version number to the next release's version number.

--- ClickBlitz/ClickBlitz.h
+++ ClickBlitz/ClickBlitz.h
@@ -11,7 +11,7 @@
 
 // Public string constants
 #define APP_NAME L"ClickBlitz"
-#define APP_VERSION L"1.0.0"
+#define APP_VERSION L"1.1.0"
 #define APP_AUTHOR L"David Jonjic"
 
 void EnableGUIInputs(bool isEnabled);
